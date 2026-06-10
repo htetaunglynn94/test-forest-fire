80e4ec79764a9e9c11aaad6d0f9274d03c16f12f
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-06-10 18:35:31 +0800

    updated

--- application.py
+++ application.py
@@ -1,40 +1,42 @@
 import pickle
+from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from flask import Flask, render_template, jsonify, request
 
 application = Flask(__name__)
-app = application
+app=application
 
-# Import Ridge Regressor and Standard Scaler
-ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
-scaler_model = pickle.load(open('models/scaler.pkl', 'rb'))
+## import ridge regresor model and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
-@app.route("/")
+## Route for home page
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/predictdata", methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method == "POST":
-        temperature = float(request.form.get('Temperature'))
-        rh = float(request.form.get('RH'))
-        ws = float(request.form.get('Ws'))
-        rain = float(request.form.get('Rain'))
-        ffmc = float(request.form.get('FFMC'))
-        dmc = float(request.form.get('DMC'))
-        isi = float(request.form.get('ISI'))
-        classes = float(request.form.get('Classes'))
-        region = float(request.form.get('Region'))
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
 
-        # Transform data
-        scaled = scaler_model.transform([[temperature,rh,ws,rain,ffmc,dmc,isi,classes,region]])
-        result = ridge_model.predict(scaled)
-        return render_template('home.html', results=round(result[0],3))
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',result=result[0])
 
     else:
         return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
