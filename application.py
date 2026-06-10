969711be609e0efba7ed76c3858e9ac6743e6279
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-06-10 19:21:58 +0800

    updated

--- application.py
+++ application.py
@@ -5,11 +5,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
-app=application
+app = application
 
 ## import ridge regresor model and standard scaler pickle
-ridge_model=pickle.load(open('models/ridge.pkl','rb'))
-standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
+ridge_model = pickle.load(open('models/ridge.pkl','rb'))
+scaler_model = pickle.load(open('models/scaler.pkl','rb'))
 
 ## Route for home page
 @app.route('/')
@@ -19,24 +19,24 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        Temperature=float(request.form.get('Temperature'))
-        RH = float(request.form.get('RH'))
-        Ws = float(request.form.get('Ws'))
-        Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC'))
-        DMC = float(request.form.get('DMC'))
-        ISI = float(request.form.get('ISI'))
-        Classes = float(request.form.get('Classes'))
-        Region = float(request.form.get('Region'))
+        temperature = float(request.form.get('Temperature'))
+        rh = float(request.form.get('RH'))
+        ws = float(request.form.get('Ws'))
+        rain = float(request.form.get('Rain'))
+        ffmc = float(request.form.get('FFMC'))
+        dmc = float(request.form.get('DMC'))
+        isi = float(request.form.get('ISI'))
+        classes = float(request.form.get('Classes'))
+        region = float(request.form.get('Region'))
 
-        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result=ridge_model.predict(new_data_scaled)
+        scaled_data = scaler_model.transform([[temperature,rh,ws,rain,ffmc,dmc,isi,classes,region]])
+        result=ridge_model.predict(scaled_data)
 
-        return render_template('home.html',result=result[0])
+        return render_template('home.html', results=round(result[0],3))
 
     else:
         return render_template('home.html')
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
